c1d84ad4db0f704253a8e629321bcc3a89491edb
vladimircardozo <vladimircardozo110@gmail.com>
2024-10-29 20:48:55 -0300

    agrego de cartDetail

--- BackEnd/src/dao/models/carts.models.js
+++ BackEnd/src/dao/models/carts.models.js
@@ -1,14 +1,15 @@
 import mongoose from 'mongoose';
 
-const productSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    description: { type: String, required: true },
-    price: { type: Number, required: true },
-    stock: { type: Number, required: true },
-    category: { type: String, required: true },
-    code: { type: String, required: true, unique: true },
+// Define a schema for individual products within a cart
+const productInCartSchema = new mongoose.Schema({
+    productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+    quantity: { type: Number, default: 1 }
+});
+
+const cartSchema = new mongoose.Schema({
+    products: [productInCartSchema]  
 }, { timestamps: true });
 
-const Product = mongoose.model('Product', productSchema);
+const Cart = mongoose.model('Cart', cartSchema);
 
-export default Product;
+export default Cart;
