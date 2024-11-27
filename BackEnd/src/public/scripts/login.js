57c9c04b028bd4c3ab30b39a66b8eb4e62bbd536
vladimircardozo <vladimircardozo110@gmail.com>
2024-11-27 10:45:17 -0300

    register/login

--- BackEnd/src/public/scripts/login.js
+++ BackEnd/src/public/scripts/login.js
@@ -1,24 +1,27 @@
-const selector = document.querySelector('#login');
+// const selector = document.querySelector('#login');
 
-selector.addEventListener('click', async (event) => {
-  try {
-    event.preventDefault();
-    const data = {
-      email: document.querySelector('#email').value,
-      password: document.querySelector('#password').value,
-    };
+// selector.addEventListener('click', async (event) => {
+//   try {
+//     event.preventDefault();
+//     const data = {
+//       email: document.querySelector('#email').value,
+//       password: document.querySelector('#password').value,
+//     };
 
-    const options = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
-    };
+//     const options = {
+//       method: 'POST',
+//       headers: { 'Content-Type': 'application/json' },
+//       body: JSON.stringify(data),
+//     };
 
-    let response = await fetch('/api/sessions/login', options);
-    response = await response.json();
-    localStorage.setItem('token', response.token);
-    alert(response.message);
-  } catch (error) {
-    alert(error.message);
-  }
-});
+//     let response = await fetch('/api/sessions/login', options);
+//     response = await response.json();
+//     if (response.message === "USER LOGGED IN") {
+//       location.replace("/")
+//   } else {
+//       alert("INVALID CREDENTIALS")
+//   }
+//   } catch (error) {
+//     alert(error.message);
+//   }
+// });
--- BackEnd/src/public/scripts/register.js
+++ BackEnd/src/public/scripts/register.js
@@ -1,31 +1,31 @@
-const selector = document.querySelector('#register');
+// const selector = document.querySelector('#register');
 
-selector.addEventListener('click', async (event) => {
-  try {
-    event.preventDefault();
-    const data = {
-      name: document.querySelector('#name').value,
-      email: document.querySelector('#email').value,
-      password: document.querySelector('#password').value,
-    };
+// selector.addEventListener('click', async (event) => {
+//   try {
+//     event.preventDefault();
+//     const data = {
+//       name: document.querySelector('#name').value,
+//       email: document.querySelector('#email').value,
+//       password: document.querySelector('#password').value,
+//     };
 
-    const options = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(data),
-    };
+//     const options = {
+//       method: 'POST',
+//       headers: { 'Content-Type': 'application/json' },
+//       body: JSON.stringify(data),
+//     };
 
-    let response = await fetch('/api/sessions/register', options);
-    console.log("Response:", response)
-    response = await response.json();
-    console.log("Response:", response);
-    if (response.status === 201) {
-      alert ('User created successfully');
-      window.location.href = './login';
-    } else {
-      alert ('Error creating user');
-    } 
-  } catch (error) {
-    alert(error.message);
-  }
-});
+//     let response = await fetch('/api/sessions/register', options);
+//     console.log("Response:", response)
+//     response = await response.json();
+//     console.log("Response:", response);
+//     if (response.status === 201) {
+//       alert ('User created successfully');
+//       window.location.href = './login';
+//     } else {
+//       alert ('Error creating user');
+//     } 
+//   } catch (error) {
+//     alert(error.message);
+//   }
+// });
