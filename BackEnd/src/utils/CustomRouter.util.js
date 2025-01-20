6010445689a24320fbc579324cacf9cd9cb4e00a
vladimircardozo <vladimircardozo110@gmail.com>
2025-01-20 18:56:34 -0300

    custom router

--- BackEnd/src/utils/CustomRouter.util.js
+++ BackEnd/src/utils/CustomRouter.util.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import jwt from 'jsonwebtoken';
 import { readById } from '../data/mongo/managers/users.manager.js';
-import argsUtil from './args.util.js';
+import envUtil from './env.util.js'
 
 class CustomRouter {
   constructor() {
@@ -32,7 +32,7 @@ class CustomRouter {
       if (policies.includes('PUBLIC')) return next();
       const token = req?.cookies?.token;
       if (!token) return res.json401();
-      const data = jwt.verify(token, argsUtil.SECRET);
+      const data = jwt.verify(token, envUtil.SECRET_KEY);
       const { role, user_id } = data;
       if (!role || !user_id) return res.json401();
       if (
